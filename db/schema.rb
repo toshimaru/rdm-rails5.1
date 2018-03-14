e3d1de9e5ca5bbdeca7d734e23e2f4a822f3e596
toshimaru <me@toshimaru.net>
2018-03-14 15:08:00 +0900

    Create Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314053417) do
+ActiveRecord::Schema.define(version: 20180314054939) do
+
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "user_id"
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", null: false
@@ -20,4 +29,5 @@ ActiveRecord::Schema.define(version: 20180314053417) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
